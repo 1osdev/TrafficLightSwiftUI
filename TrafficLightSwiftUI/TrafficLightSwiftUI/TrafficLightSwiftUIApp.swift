446e7233b35e82464cbcbd40fea97cb44c4d9787
DMITRY SMIRNOV <smirnoff145@gmail.com>
2022-03-30 23:20:15 +0300

    project created

---
+++ TrafficLightSwiftUI/TrafficLightSwiftUI/TrafficLightSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightSwiftUIApp.swift
+//  TrafficLightSwiftUI
+//
+//  Created by Дмитрий Смирнов on 30.03.22.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
